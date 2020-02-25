4d6ef0d5bfcd7bfb7c7fb9faad4cfbae7cc486d0
dspages <dspages@gmail.com>
2020-02-24 23:10:09 -0800

    Basic setup of planet scene with orbiter and lander and camera

---
+++ Assets/Scripts/Planet/OrbitingObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitingObject : MonoBehaviour
+{
+    [SerializeField]
+    public float angle = 0.0f;
+    private float radius = 6.0f;
+    private float speed = -0.001f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += speed;
+        transform.position = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * radius;
+    }
+}
---
+++ Assets/Scripts/Planet/PlanetCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetCam : MonoBehaviour
+{
+    [SerializeField]
+    GameObject toCenter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(toCenter.transform.position.x, toCenter.transform.position.y, -10f);
+        float angle = Mathf.Atan2(transform.position.y, transform.position.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
