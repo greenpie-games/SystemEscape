678f062195812f90ab9a0f00a322721ed3b431b9
dspages <dspages@gmail.com>
2020-03-26 00:01:02 -0700

    Added lander exhaust, limited fuel, and crash detection

--- Assets/Scripts/Planet/Lander.cs
+++ Assets/Scripts/Planet/Lander.cs
@@ -9,15 +9,27 @@ public class Lander : MonoBehaviour
     [SerializeField]
     Text instructionalText;
 
+    [SerializeField]
+    ParticleSystem exhaust;
+
+    [SerializeField]
+    Image fuelIndicator;
+
+    const float maxFuel = 500f;
+    private float fuel = 500f;
+    float planetRadius = 2.4f;
+
     enum state
     {
         attachedToOrbiter,
         playerControlled,
-        landing
+        landed,
+        crashed
     };
 
     private state currentState;
     Vector2 velocity;
+    public float absVelocity;
 
     void PhysUpdate()
     {
@@ -25,11 +37,24 @@ public class Lander : MonoBehaviour
         velocity.x -= transform.position.normalized.x * 0.000006f;
         velocity.y -= transform.position.normalized.y * 0.000006f;
         transform.position = (Vector2)transform.position + velocity;
+        absVelocity = Mathf.Sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
+        if (Mathf.Sqrt(transform.position.x * transform.position.x + transform.position.y * transform.position.y) < planetRadius)
+        {
+            if (absVelocity > .001f)
+            {
+                currentState = state.crashed;
+            }
+            else
+            {
+                currentState = state.landed;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        var exhaustEmission = exhaust.emission;
         if (currentState == state.attachedToOrbiter)
         {
             instructionalText.text = "Press space bar to deorbit";
@@ -43,13 +68,32 @@ public class Lander : MonoBehaviour
         else if (currentState == state.playerControlled)
         {
             instructionalText.text = "WAD to move...";
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) && fuel > 0)
+            {
                 velocity += (Vector2)(0.000025f * transform.up);
+                exhaustEmission.enabled = true;
+                fuel -= 1f;
+                fuelIndicator.fillAmount = fuel / maxFuel;
+            }
+            else
+            {
+                exhaustEmission.enabled = false;
+            }
             if (Input.GetKey(KeyCode.A))
-                transform.Rotate(Vector3.forward * 0.5f);
+                transform.Rotate(Vector3.forward * 0.75f);
             if (Input.GetKey(KeyCode.D))
-                transform.Rotate(Vector3.forward * -0.5f);
+                transform.Rotate(Vector3.forward * -0.75f);
             PhysUpdate();
+        }
+        else if (currentState == state.landed)
+        {
+            instructionalText.text = "Successful landing!";
+            exhaustEmission.enabled = false;
+        }
+        else if (currentState == state.crashed)
+        {
+            instructionalText.text = "Crash! Game over.";
+            exhaustEmission.enabled = false;
         }
     }
 }
